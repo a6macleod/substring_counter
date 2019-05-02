15ff4210a09e095fc5d4ded60ae7536a1cbf99cc
Andrew MacLeod <A6macleod@gmail.com>
2019-05-02 16:06:02 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # substring_counter
 A counter to take in words and break them down into substrings and count them.
+
+This project is from The Odin Project https://www.theodinproject.com/courses/ruby-programming/lessons/building-blocks?ref=lnav
---
+++ substing_counter.rb
@@ -0,0 +1,13 @@
+# May 2, 2019
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+print "Enter a string and I'll count the stubstrings: "
+user_input = gets.chomp
+
+def substings
+	puts user_input
+	
+end
+
+substrings(user_input, dictionary)
